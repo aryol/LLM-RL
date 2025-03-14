b54a0d0fb94e5685a02cbd63c25101bf3f162d63
Aryo Lotfi <me@aryolotfi.com>
2025-03-14 18:12:21 +0100

    server setup file

---
+++ install_script.sh
@@ -0,0 +1,28 @@
+# Install Pytorch & other libraries, make sure to match your GPU driver version
+pip install -U tensorboard "setuptools<71.0.0"
+ 
+# Install flash-attn
+pip install -U flash-attn 
+ 
+# Install Hugging Face libraries
+pip install  --upgrade \
+  "transformers==4.48.1" \
+  "datasets==3.1.0" \
+  "accelerate==1.3.0" \
+  "hf-transfer==0.1.9" \
+  "deepspeed==0.15.4" \
+  "trl==0.14.0"
+ 
+
+pip uninstall pynvml
+pip install -U nvidia-ml-py
+pip install wandb
+
+# install vLLM 
+pip install -U vllm
+pip install -U trl
+ 
+## IMPORTANT: If you want to run the notebook and the interactive cells you also need to install the following libraries:
+# But first read it the blog post and then decide as they might conflict with the libraries for distributed training. 
+# %pip install "peft==0.14.0" "bitsandbytes==0.45.0"
+ 
