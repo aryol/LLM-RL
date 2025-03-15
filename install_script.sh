75e496f4a91e6db61a69530e13278d1f32a14bf0
Aryo Lotfi <me@aryolotfi.com>
2025-03-16 00:56:31 +0100

    handling job submission non interactive

--- install_script.sh
+++ install_script.sh
@@ -13,16 +13,16 @@ pip install  --upgrade \
   "datasets==3.1.0" \
   "accelerate==1.3.0" \
   "hf-transfer==0.1.9" \
-  "deepspeed==0.15.4" \
-  "trl==0.15.2"
+  "deepspeed==0.15.4"
  
 
-pip uninstall pynvml
+pip uninstall -y pynvml
 pip install -U nvidia-ml-py
 pip install wandb
 
 # install vLLM 
 pip install -U vllm
+pip install git+https://github.com/huggingface/trl.git
 # pip install -U trl
  
 ## IMPORTANT: If you want to run the notebook and the interactive cells you also need to install the following libraries:
