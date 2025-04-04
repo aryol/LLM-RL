9848ec26db2a25ab5567f879b4442f0866a45c0f
Aryo Lotfi <me@aryolotfi.com>
2025-04-04 13:53:49 +0200

    Some edits mostly to config files for the PPO experiment

--- scripts/run1gpu.sh
+++ scripts/run1gpu.sh
@@ -3,4 +3,7 @@ source /dlabscratch1/amani/miniconda3/bin/activate
 conda activate LLM-RL
 
 # Run the training script
-python src/train.py experiment=adaptive model=tinyllama_1b task=gsm8k dataset_wrapper.reward_threshold=0.125 
+# python src/train.py experiment=adaptive model=tinyllama_1b task=gsm8k dataset_wrapper.reward_threshold=0.125 
+
+python src/train.py experiment=PPO model=gpt2xl_1epochsft task=gsm8k \
+wandb_config.name=Qwen0.5Bdebug dataset_wrapper=adaptive dataset_wrapper.reward_threshold=0.12
