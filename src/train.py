39dd6c3ce53093f522487bb648b57ab52add7367
Aryo Lotfi <me@aryolotfi.com>
2025-04-10 15:13:26 +0200

    small fixes for PPO

--- src/train.py
+++ src/train.py
@@ -76,10 +76,10 @@ def train(config):
     # take a portion of training data for validation
     # train_dataset, val_dataset = dataset["train"].train_test_split(test_size=0.1, seed=42).values()
     train_dataset = dataset["train"].shuffle(seed=42)
-    test_dataset = dataset["test"].shuffle(seed=42)
+    # test_dataset = dataset["test"].shuffle(seed=42)
     # only sample few samples for debugging
     # train_dataset = dataset["train"].select(range(64))
-    # test_dataset = dataset["test"].select(range(64))
+    test_dataset = dataset["test"].select(range(64))
 
     generate_prompt = hydra.utils.get_method(config.generate_prompt)(config, tokenizer=tokenizer)
     train_dataset = hydra.utils.instantiate(config.dataset_wrapper, dataset=train_dataset, generate_prompt=generate_prompt, initial_portion=0.0, prompt_key=config.task.prompt_key, target_key=config.task.target_key)
