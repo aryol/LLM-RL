b083e70bef1f629e3aee94b55d78e6e42094e4fe
mh-amani <mh.amani1998@gmail.com>
2025-04-25 10:59:03 +0000

    sft upload glitch

--- scripts/sft_on_dataset/run.sh
+++ scripts/sft_on_dataset/run.sh
@@ -4,7 +4,7 @@
 
 # set -x
 
-# to train:
+# # to train:
 # torchrun --nproc_per_node=1 scripts/sft_on_dataset/train.py \
 #     data.name=math \
 #     data.max_length=3072 \
@@ -13,7 +13,7 @@
 #     data.train_batch_size=256 \
 #     trainer.total_epochs=2
     
-# to just upload models from the above training to hub:
+# # to just upload models from the above training to hub:
 # torchrun --nproc_per_node=1 scripts/sft_on_dataset/train.py \
-#     just_upload_models_to_hub=True \
-#     path_to_checkpoints_folder=/dlabscratch1/amani/LLM-RL/logs/SFT_for_rl/math_Llama-2-7b-hf/2025-04-24_20-57-06
+#     +just_upload_models_to_hub=True \
+#     +path_to_checkpoints_folder=/dlabscratch1/amani/prod/LLM-RL/logs/SFT_for_rl/gsm8k_Llama-2-7b-hf/2025-04-24_20-49-32/checkpoints/
