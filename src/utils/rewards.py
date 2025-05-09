198bb79815820c27ea859f98acec4f1071ec193b
Aryo Lotfi <me@aryolotfi.com>
2025-05-09 14:03:45 +0200

    corrected the reward func for tensor GSM

--- src/utils/rewards.py
+++ src/utils/rewards.py
@@ -1,3 +1,8 @@
+import re
+
+def extract_initial_number(s):
+    match = re.match(r'\s*(\d+)', s)
+    return int(match.group(1)) if match else -1234
 
 def ParityReward(data_source, solution_str, ground_truth, extra_info=None):
     ground_truth_list = ground_truth.split()
@@ -71,17 +76,18 @@ def CumulativeParityReward(data_source, solution_str, ground_truth, extra_info=N
 
 def TensorGSMReward(data_source, solution_str, ground_truth, extra_info=None):
     ground_truth_list = ground_truth.split(' and ')
-    length = len(ground_truth_list)
 
     try:
         solution_list = solution_str.split('####')[1:]
     except:
         return 0
-    if len(solution_list) != len(ground_truth_list):
+    
+    if len(solution_list) < len(ground_truth_list):
         return 0
+    
     for i in range(len(solution_list)):
         try:
-            if int(solution_list[i].strip()) != int(ground_truth[i].strip()):
+            if extract_initial_number(solution_list[i]) != int(ground_truth_list[i].strip()):
                 return 0.1
         except:
             return 0.1
